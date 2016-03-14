a592f35216bfae0c6dfee113b0a46adef8494362
Andre Valdestilhas <firmao@gmail.com>
2016-03-14 09:24:19 +0100

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,20 +1,11 @@
-import java.io.File;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.IntStream;
 
-import aksw.sim.proof.AndreMFKC;
-import aksw.sim.proof.ExperimentMFKC;
+import java.io.File;
+
 
 public class Testing {
 	public static void main(String[] args) {
-		File ds = new File("D:\\task_axel1\\dbPediaPlaces.tsv");
-		File dt = new File("D:\\task_axel1\\linkedGeoPlaces_million.tsv");
+		File ds = new File("dbPediaPlaces.tsv");
+		File dt = new File("linkedGeoPlaces_million.tsv");
 		int k=200;
 		double threshold=0.9d;
 		//32=1,000 -  105=10,000 - 343=100,000 - 1249=1,000,000 - 3900=10,000,000
